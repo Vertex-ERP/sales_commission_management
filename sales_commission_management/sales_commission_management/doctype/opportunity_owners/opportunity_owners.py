b99652341db6b54dbb75f2c51188c3f2380ea4ec
AbeerSwadi <abeerswadi280@gmail.com>
2024-12-11 20:43:54 +0100

    add more than oppertunity owner

---
+++ sales_commission_management/sales_commission_management/doctype/opportunity_owners/opportunity_owners.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, YemenFrappe and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class OpportunityOwners(Document):
+	pass
