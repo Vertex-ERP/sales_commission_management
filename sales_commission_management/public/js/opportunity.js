2e30ffd69b169a939718a2c50437ea998640f8e9
alaalsalam <alaalsalam101@gmail.com>
2024-12-24 09:54:10 +0000

    add  item that the researcher searched for

--- sales_commission_management/public/js/opportunity.js
+++ sales_commission_management/public/js/opportunity.js
@@ -1,25 +1,25 @@
-// frappe.ui.form.on('CRM Note', {
-//     approve: function(frm, cdt, cdn) {
-//         let row = frappe.get_doc(cdt, cdn);
 
-//         if (row.approve === 1) {
-
-//             frm.set_value('yf_researcher', row.added_by);
-//             frm.refresh_field('yf_researcher');
+frappe.ui.form.on('Opportunity', {
+    setup: function(frm) {
+    frm.fields_dict["notes"].grid.get_field("yf_custom_item").get_query = function(doc, cdt, cdn) {
+     let d = locals[cdt][cdn];
+     return {
+      query: "sales_commission_management.sales_commission_management.doctype.api.get_item",
+      filters: {
+        "added_by": d.added_by,
+                                }
+     };
+    }
+   }})
+   frappe.ui.form.on('CRM Note', {
+    added_by: function(frm, cdt, cdn) {
+      var row = locals[cdt][cdn]; 
+      if (row.added_by) {
+            var unique_id = 'AD-' + new Date().toISOString().replace(/[-:]/g, '').split('.')[0];
+            console.log(unique_id)
             
+            frappe.model.set_value(cdt, cdn, 'yf_added_by_id', unique_id);
+        }
+    }
+});
 
-//             frm.doc.notes.forEach((item) => {
-//                 if (item.name !== row.name && item.approve === 1) {
-//                     item.approve = 0;
-//                 }
-//             });
-
-//             frm.refresh_field('notes');
-
-//         } else {
-
-//             frm.set_value('yf_researcher', null); 
-//             frm.refresh_field('yf_researcher');
-//         }
-//     }
-// });
